b27bf12dd7c4a23171738dd061a83084d2ed5101
板板 <747657490@qq.com>
2025-06-05 17:54:44 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,16 @@
           <Popup v-model="showPopup" class="landing_mobile_nav">
             <ul>
               <li
-              v-for="(item, index) in companyList"
-              :key="index"
+                v-for="(item, index) in companyList"
+                :key="index"
               ><a href="#">{{item}}</a></li>
             </ul>
           </Popup>
           <ul class="landing_desktop_nav">
-            <li></li>
+            <li
+              v-for="(item, index) in companyList"
+              :key="index"
+            ><a href="#">{{item}}</a></li>
           </ul>
         </nav>
         <button class="landing_show_nav" @click="showPopup=true">open nav</button>
@@ -22,23 +25,36 @@
     </header>
     <main>
       <article class="landing_intro">
-        <img class="landing_intro_img" src="./assets/images/mobile/image-interactive.jpg" alt="">
-        <h3 class="landing_intro_title">THE LEADER IN INTERACTIVE VR</h3>
-        <p class="landing_intro_msg">
-          Founded in 2011, Loopstudios has been producing world-class virtual reality
-          projects for some of the best companies around the globe. Our award-winning
-          creations have transformed businesses through digital experiences that bind
-          to their brand.
-        </p>
+        <div class="center">
+          <img class="landing_intro_img" src="./assets/images/mobile/image-interactive.jpg" alt="">
+          <div class="landing_intro_card">
+            <h3 class="landing_intro_title">THE LEADER IN INTERACTIVE VR</h3>
+            <p class="landing_intro_msg">
+              Founded in 2011, Loopstudios has been producing world-class virtual reality
+              projects for some of the best companies around the globe. Our award-winning
+              creations have transformed businesses through digital experiences that bind
+              to their brand.
+            </p>
+          </div>
+        </div>
       </article>
       <section class="landing_creations">
         <h3 class="landing_creations_title">OUR CREATIONS</h3>
-        <ul class="landing_creations_card">
+        <ul class="landing_creations_card_mobile">
           <li
           v-for="(item, index) in creationCard"
           :key="index"
           >
-            <div class="landing_creations_img"><img :src="item.image" alt=""></div>
+            <div class="landing_creations_img"><img :src="item.image1" alt=""></div>
+            <p class="landing_creations_card_des">{{item.line1}}<br>{{item.line2}}</p>
+          </li>
+        </ul>
+        <ul class="landing_creations_card_desktop">
+          <li
+          v-for="(item, index) in creationCard"
+          :key="index"
+          >
+            <div class="landing_creations_img"><img :src="item.image2" alt=""></div>
             <p class="landing_creations_card_des">{{item.line1}}<br>{{item.line2}}</p>
           </li>
         </ul>
@@ -46,29 +62,31 @@
       </section>
     </main>
     <footer>
-      <div class="landing_company_left">
-        <h4 class="landing_company_logo"></h4>
-        <ul class="landing_company">
-          <li
-          class="landing_company_item"
-          v-for="(item, index) in companyList"
-          :key="index"
-          ><a href="#"></a>{{item}}</li>
-        </ul>
-      </div>
-      <div class="landing_share_right">
-        <ul>
-          <li
-            v-for="(item, index) in shareList"
+      <div class="container">
+        <div class="landing_company_left">
+          <h4 class="landing_company_logo"></h4>
+          <ul class="landing_company">
+            <li
+            class="landing_company_item"
+            v-for="(item, index) in companyList"
             :key="index"
-          >
-            <a href="#">
-              <img :src="item" alt="">
-            </a>
-          </li>
-        </ul>
-        <div class="landing_copyright">
-          © 2021 Loopstudios. All rights reserved.
+            ><a href="#"></a>{{item}}</li>
+          </ul>
+        </div>
+        <div class="landing_share_right">
+          <ul>
+            <li
+              v-for="(item, index) in shareList"
+              :key="index"
+            >
+              <a href="#">
+                <img :src="item" alt="">
+              </a>
+            </li>
+          </ul>
+          <div class="landing_copyright">
+            © 2021 Loopstudios. All rights reserved.
+          </div>
         </div>
       </div>
     </footer>
@@ -87,42 +105,50 @@ export default {
       // our creations
       creationCard: [
         {
-          image: require('./assets/images/mobile/image-deep-earth.jpg'),
+          image1: require('./assets/images/mobile/image-deep-earth.jpg'),
+          image2: require('./assets/images/desktop/image-deep-earth.jpg'),
           line1: 'DEEP',
           line2: 'EARTH'
         },
         {
-          image: require('./assets/images/mobile/image-night-arcade.jpg'),
+          image1: require('./assets/images/mobile/image-night-arcade.jpg'),
+          image2: require('./assets/images/desktop/image-night-arcade.jpg'),
           line1: 'NIGHT',
           line2: 'ARCADE'
         },
         {
-          image: require('./assets/images/mobile/image-soccer-team.jpg'),
+          image1: require('./assets/images/mobile/image-soccer-team.jpg'),
+          image2: require('./assets/images/desktop/image-soccer-team.jpg'),
           line1: 'SOCCER',
           line2: 'TEAM VR'
         },
         {
-          image: require('./assets/images/mobile/image-grid.jpg'),
+          image1: require('./assets/images/mobile/image-grid.jpg'),
+          image2: require('./assets/images/desktop/image-grid.jpg'),
           line1: 'THE',
           line2: 'GRID'
         },
         {
-          image: require('./assets/images/mobile/image-from-above.jpg'),
+          image1: require('./assets/images/mobile/image-from-above.jpg'),
+          image2: require('./assets/images/desktop/image-from-above.jpg'),
           line1: 'FROM UP',
           line2: 'ABOVE VR'
         },
         {
-          image: require('./assets/images/mobile/image-pocket-borealis.jpg'),
+          image1: require('./assets/images/mobile/image-pocket-borealis.jpg'),
+          image2: require('./assets/images/desktop/image-pocket-borealis.jpg'),
           line1: 'POCKET',
           line2: 'BOREALIS'
         },
         {
-          image: require('./assets/images/mobile/image-curiosity.jpg'),
+          image1: require('./assets/images/mobile/image-curiosity.jpg'),
+          image2: require('./assets/images/desktop/image-curiosity.jpg'),
           line1: 'THE',
           line2: 'CURIOSITY'
         },
         {
-          image: require('./assets/images/mobile/image-fisheye.jpg'),
+          image1: require('./assets/images/mobile/image-fisheye.jpg'),
+          image2: require('./assets/images/desktop/image-fisheye.jpg'),
           line1: 'MAKE IT',
           line2: 'FISHEYE'
         }
